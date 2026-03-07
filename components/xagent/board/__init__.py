c034c282b6734ae95f9d56bf22e8f05b7601b98f
Xuelin Wang <xuelin.wang@sonatus.com>
2026-03-07 12:16:10 -0800

    more tetris logic/tests

--- components/xagent/board/__init__.py
+++ components/xagent/board/__init__.py
@@ -1,3 +1,4 @@
-from xagent.board import core
+from xagent.board.clear_rows import clear_rows
+from xagent.board.core import empty_board, set_piece, set_cell
 
-__all__ = ["core"]
+__all__ = ["empty_board", "set_piece", "set_cell", "clear_rows"]
